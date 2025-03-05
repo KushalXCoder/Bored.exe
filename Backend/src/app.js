e8faab59a97a09af5edba54461d5e0aba371fc39
Kushal Rathod <168859142+KushalXCoder@users.noreply.github.com>
2025-03-05 23:17:27 +0530

    Add files via upload

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -24,17 +24,17 @@ app.get("/getData", async (req,res) => {
         const response = await getData(option);
         res.status(200).json(response);
     } catch (error) {
-        res.status(400).send("Error fetching data", error);
+        res.status(400).json({ message: "Error fetching data", error: error.message });
     }
 })
 
-app.get("/getNews", async (req,res) => {
-    try {
-        const response = await getNews();
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(400).send("Error fetching news", error);
-    }
-})
+// app.get("/getNews", async (req,res) => {
+//     try {
+//         const response = await getNews();
+//         res.status(200).json(response);
+//     } catch (error) {
+//         res.status(400).json({ message: "Error fetching news", error: error.message });
+//     }
+// })
 
 export default app;
